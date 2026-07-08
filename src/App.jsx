e3c7d0e4d7e940ae4bee1d62be68390585444608
A6h1ramahy <abhiramhy836@gmail.com>
2026-07-08 22:48:09 +0530

    minor update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -7,10 +7,21 @@ import ScrollProgress from './components/ScrollProgress';
 import ProblemStatement from './pages/ProblemStatement';
 import TechnicalDocs from './pages/TechnicalDocs';
 
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [location.pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <div className="min-h-screen flex flex-col bg-light-bg dark:bg-dark-bg text-stone-900 dark:text-stone-100 transition-colors duration-300">
           <Navbar />
           <ScrollProgress />
